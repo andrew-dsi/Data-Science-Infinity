781b591e2b7d4188721c64358a015669a45f9a53
analytics-link <admin@analtyics-link.com>
2023-11-24 14:18:57 +0000

    Initial commit

--- Docker/Docker-Fundamentals/src/hello-faked-person.py
+++
@@ -1,6 +0,0 @@
-import faker
-
-Faker = faker.Factory().create
-fake = Faker()
-
-print(f"Hello {fake.name()} from {fake.state()}")
--- Docker/Docker-Fundamentals/src/hello-world.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# if we pass an argument we print it, if not we just print hello world
-if len(sys.argv) > 1:
-    print(f"hello-world {sys.argv[1]}")
-else:
-    print(f"hello-world")
-
